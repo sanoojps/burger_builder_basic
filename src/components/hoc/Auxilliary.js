93590da0ed44da7fd1caf215658d204c3d939f74
sanoojps <sanojneo@yahoo.co.in>
2018-09-15 21:46:11 +0530

    Starting Burger Builder

---
+++ src/components/hoc/Auxilliary.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Component to wrap adjacent JSX components in  
+ * a component 
+ */
+
+ /**
+  * 
+  * @param {props from the wrapping JSX} props 
+  */
+const aux = (props) => {
+    return props.children 
+}
+
+export default aux
